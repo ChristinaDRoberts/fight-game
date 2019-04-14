b68d2d9c5878404f831e39a0804a699adbfb97f8
Christina <Christinepeninsula@hotmail.com>
2019-04-14 14:02:57 -0400

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'fight/static')
 STATIC_URL = './fight/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
@@ -29,9 +29,9 @@ STATICFILES_FINDERS = [
 ]
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
 
 
 # Quick-start development settings - unsuitable for production
